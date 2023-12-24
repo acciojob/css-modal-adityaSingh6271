d31cc115b57760e058cfa32745125d95565c0829
Aditya Singh <131813027+adityaSingh6271@users.noreply.github.com>
2023-12-24 12:56:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const openModalBtn = document.getElementById('openModal');
+const closeModalBtn = document.getElementById('closeModal');
+const modal = document.getElementById('modal');
+
+openModalBtn.addEventListener('click', function() {
+  modal.style.display = 'flex';
+});
+
+closeModalBtn.addEventListener('click', function() {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
